950d141fafa75b396abb1ace3fdeae91bbe9e162
grafst <grafst@users.noreply.github.com>
2024-03-25 21:32:48 +0100

    formatting and requirements

--- isopen.py
+++ isopen.py
@@ -2,16 +2,20 @@ import time
 import datetime
 from abc import ABC
 
+
 class OpeningHours:
-    def _init_(self, last_updated):
+    def __init__(self, last_updated):
         self.last_updated = last_updated
         self.timeslots = []
+
     def add_timeslot(self, new_timeslot):
         self.timeslots.append(new_timeslot)
+
     def get_opening_hours(self):
         # calculate union of positive slots minus union of negative slots
         # return this as a list of intervals when place is open
         pass
+
     def is_open_now(self):
         timestamp_now = time.time()
         opening_hours = self.get_opening_hours()
@@ -19,46 +23,58 @@ class OpeningHours:
             if timestamp_from <= timestamp_now <= timestamp_to:
                 return True
         return False
+
     def next_change(self):
         pass
+
     def get_remaining_time(self):
         pass
         # use method next_change() to implement this
         # i.e. next_change() - now
 
+
 class OpeningTimeslot(ABC):
     # NOTE: Timeslots may only be *continuous* time intervals!
     def is_positive(self):
         pass
+
     def open_intervall_24h(self):
         pass
 
+
 class AlwaysTimeslot(OpeningTimeslot):
-    def _init_(self, is_positive):
+    def __init__(self, is_positive):
         self.is_positive = is_positive
+
     def _str_(self):
         string_repr = "24/7"
         if not self.is_positive:
             string_repr += " off"
         return string_repr
+
     def is_positive(self):
         return self.is_positive
+
     def open_intervall_24h(self):
         timestamp_from = time.time()
         seconds_in_24_hours = 24 * 60 * 60
         timestamp_to = int(timestamp_from + seconds_in_24_hours)
         return timestamp_from, timestamp_to
 
+
 class SunriseSunsetTimeslot(OpeningTimeslot):
-    def _init_(self, is_positive):
+    def __init__(self, is_positive):
         self.is_positive = is_positive
+
     def _str_(self):
         string_repr = "sunrise-sunset"
         if not self.is_positive:
             string_repr += " off"
         return string_repr
+
     def is_positive(self):
         return self.is_positive
+
     def open_intervall_24h(self):
         """
         import astral # use version 1.10.1
@@ -74,19 +90,23 @@ class SunriseSunsetTimeslot(OpeningTimeslot):
         """
         pass
 
+
 class SingleWeekdayTimeslot(OpeningTimeslot):
-    def _init_(self, weekday, time_from, time_to, is_positive):
+    def __init__(self, weekday, time_from, time_to, is_positive):
         self.weekday = weekday
         self.time_from = time_from
         self.time_to = time_to
         self.is_positive = is_positive
+
     def _str_(self):
         string_repr = self.weekday + " " + self.time_from + "-" + self.time_to
         if not self.is_positive:
             string_repr += " off"
         return string_repr
+
     def is_positive(self):
         return self.is_positive
+
     def open_intervall_24h(self):
         WEEKDAY_ABBREVIATIONS = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
         current_date = datetime.datetime.now().date()
@@ -103,33 +123,41 @@ class SingleWeekdayTimeslot(OpeningTimeslot):
             timestamp_to = timestamp_max
         return timestamp_from, timestamp_to
 
+
 class SingleMonthTimeslot(OpeningTimeslot):
-    def _init_(self, month, time_from, time_to, is_positive):
+    def __init__(self, month, time_from, time_to, is_positive):
         self.month = month
         self.time_from = time_from
         self.time_to = time_to
         self.is_positive = is_positive
+
     def _str_(self):
         return self.month + " " + self.time_from + "-" + self.time_to
+
     def is_positive(self):
         return self.is_positive
+
     def open_intervall_24h(self):
         pass
 
+
 class SingleMonthdayTimeslot(OpeningTimeslot):
-    def _init_(self, month, monthday, time_from, time_to, is_positive):
+    def __init__(self, month, monthday, time_from, time_to, is_positive):
         self.month = month
         self.monthday = monthday
         self.time_from = time_from
         self.time_to = time_to
         self.is_positive = is_positive
+
     def _str_(self):
         string_repr = self.month + " " + self.monthday + " " + self.time_from + "-" + self.time_to
         if not self.is_positive:
             string_repr += " off"
         return string_repr
+
     def is_positive(self):
         return self.is_positive
+
     def open_intervall_24h(self):
         time_to = self.time_to
         if time_to == "24:00":
@@ -151,13 +179,16 @@ class SingleMonthdayTimeslot(OpeningTimeslot):
             timestamp_to = timestamp_max
         return timestamp_from, timestamp_to
 
+
 class HolidayTimeslot(OpeningTimeslot):
     pass
+
 
 class OpeningHoursParser(ABC):
     @staticmethod
     def get_timeslot(opening_hours_string):
         pass
+
 
 class OSM_Parser(OpeningHoursParser):
     # TODO: use numeric definition instead of such lists outside of this parser class
@@ -215,7 +246,8 @@ class OSM_Parser(OpeningHoursParser):
                             day_from = subparts[1].split("-")[0]
                             day_to = subparts[1].split("-")[1]
                             for current_day in range(int(day_from), int(day_to) + 1):
-                                timeslots.append(SingleMonthdayTimeslot(month, str(current_day), "00:00", "24:00", is_positive))
+                                timeslots.append(
+                                    SingleMonthdayTimeslot(month, str(current_day), "00:00", "24:00", is_positive))
                     else:
                         subpart_count = len(subparts)
                         if subpart_count > 2:
@@ -224,7 +256,8 @@ class OSM_Parser(OpeningHoursParser):
                             time_from = subparts[2].split("-")[0]
                             time_to = subparts[2].split("-")[1]
                             for current_day in range(int(day_from), int(day_to) + 1):
-                                timeslots.append(SingleMonthdayTimeslot(month, str(current_day), time_from, time_to, is_positive))
+                                timeslots.append(
+                                    SingleMonthdayTimeslot(month, str(current_day), time_from, time_to, is_positive))
                         else:
                             time_from = subparts[1].split("-")[0]
                             time_to = subparts[1].split("-")[1]
@@ -239,29 +272,32 @@ class OSM_Parser(OpeningHoursParser):
                         weekday_to_index = OSM_Parser.WEEKDAY_ABBREVIATIONS.index(weekday_to)
                         if weekday_from_index > weekday_to_index:
                             weekday_from_index, weekday_to_index = weekday_to_index, weekday_from_index
-                        weekdays_list = OSM_Parser.WEEKDAY_ABBREVIATIONS[weekday_from_index : weekday_to_index + 1]
+                        weekdays_list = OSM_Parser.WEEKDAY_ABBREVIATIONS[weekday_from_index: weekday_to_index + 1]
                         for current_weekday in weekdays_list:
                             OSM_Parser.__add_single_weekday(timeslots, current_weekday, subparts[1], is_positive)
                     elif subsubparts[0] in OSM_Parser.MONTH_ABBREVIATIONS:
-                        #TODO: handle multi-month and multi-day combinations
+                        # TODO: handle multi-month and multi-day combinations
                         month_from = subsubparts[0]
                         month_to = subsubparts[1]
                         month_from_index = OSM_Parser.MONTH_ABBREVIATIONS.index(month_from)
                         month_to_index = OSM_Parser.MONTH_ABBREVIATIONS.index(month_to)
                         if month_from_index > month_to_index:
                             month_from_index, month_to_index = month_to_index, month_from_index
-                        months_list = OSM_Parser.MONTH_ABBREVIATIONS[month_from_index : month_to_index + 1]
+                        months_list = OSM_Parser.MONTH_ABBREVIATIONS[month_from_index: month_to_index + 1]
                         for current_month in months_list:
                             raise NotImplementedError
                     else:
                         raise NotImplementedError
                 elif subparts[0] == OSM_Parser.HOLIDAY_INDICATOR:
-                    #return HolidayTimeslot(...)
+                    # return HolidayTimeslot(...)
                     raise NotImplementedError
         return timeslots
 
 
-result = OSM_Parser.get_timeslot("24/7; Mar 15:00-17:00; sunrise-sunset; Aug 31 off; Jan 17-23 09:33-17:45; Mo 10:00-20:00; We 08:00-02:00; Tu-Fr 08:00-12:00,12:30-15:00 off")
-for ts in result:
-    print(ts)
-    print(ts.open_intervall_24h())
+if __name__ == "__main__":
+    result = OSM_Parser.get_timeslot(
+        "24/7; Mar 15:00-17:00; sunrise-sunset; Aug 31 off; Jan 17-23 09:33-17:45; Mo 10:00-20:00; We 08:00-02:00; "
+        "Tu-Fr 08:00-12:00,12:30-15:00 off")
+    for ts in result:
+        print(ts)
+        print(ts.open_intervall_24h())
